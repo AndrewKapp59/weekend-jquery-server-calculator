6a13a3c8e60e7cddf64bf1c00a04174d3454b42f
Andrew Kapp <dkapp59@gmail.com>
2022-02-04 21:51:10 -0600

    Able to add button inputs to the object that will be posted to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,40 +1,79 @@
 $(document).ready(onReady);
 
 function onReady(){
-$('#equal').on('click', addEquation)
+  // $('.button').on('click', buttonClick)
+  // $('.button').on('click', buttonText)
+  $('#divide').on('click', divideClicked)
+  $('#multiply').on('click', multiplyClicked)
+  $('#subtract').on('click', subtractClicked)
+  $('#add').on('click', addClicked)
+  $('#equal').on('click', addEquation)
 }
 
-buttonClicked = ;
+let buttonClicked;
+
+function divideClicked () {
+  buttonClicked = '/'
+  console.log(buttonClicked);
+}
+function multiplyClicked () {
+  buttonClicked = '*'
+  console.log(buttonClicked);
+}
+function subtractClicked () {
+  buttonClicked = '-'
+  console.log(buttonClicked);
+}
+function addClicked () {
+  buttonClicked = '+'
+  console.log(buttonClicked);
+}
+
 
 function addEquation() {
   let firstInput = $('#inputOne').val();
-  // let operator = $('')
+  let operator = buttonClicked;
   let secondInput = $('#inputTwo').val();
   $('#inputOne').val('');
   $('#inputTwo').val('');
-  //use AJAX to make a post request to the server
+  //using AJAX to send a post request to the server
   $.ajax({
     method: 'POST', 
     url: '/equation',
     data: {
       equationToAdd: {
         firstNumber: firstInput,
-        // operator: '',
+        operator: operator,
         secondNumber: secondInput,
       }
     }
   }).then(function(response){
     console.log('Equation Added');
-    getInventory(); // to refresh the DOM with the new item
+    // getInventory(); // to refresh the DOM with the new item
   }).catch(function(response){
     console.log('UGHHHH Equation not added');
   })
 }
 
- function buttonClick(){
-   if($('.button').on('click').text() === '/')
- 
-   $('.button').on('click').text() === '*';
-   $('.button').on('click').text();
-   $('.button').on('click').text();
- }
+// function buttonText () {
+//   let buttonPressed = $('.button').on('click').text()
+//   console.log(buttonPressed);
+// }
+
+
+// .button on click text ends up equalling all buttons pressed at once
+
+// function buttonClick(){
+//   if($('#divide').on('click').text() === '/'){
+//     buttonClicked =  '/';
+//   }
+//   else if ($('.button').on('click').text() === '*') {
+//     buttonClicked = '*'; 
+//   }
+//   else if ($('.button').on('click').text() === '-') {
+//     buttonClicked = '-'; 
+//   }
+//   else if ($('.button').on('click').text() === '+') {
+//     buttonClicked = '-'; 
+//   }
+// }
