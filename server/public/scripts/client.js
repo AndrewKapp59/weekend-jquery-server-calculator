b1530d08055359231e80336c3f5433ac10924cbf
Andrew Kapp <dkapp59@gmail.com>
2022-02-06 14:07:40 -0600

    cleaned up the code and  formatting

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,120 +1,121 @@
 $(document).ready(onReady);
 
-function onReady(){
-  $('#equal').on('click', postEquation)
-  $('#clear').on('click', clearInputs)
-  $('#clearHistory').on('click', deleteMathHistory)
-  $('#backspace').on('click', backspace)
-  $('#percentage').on('click', percent)
-  $('#divide').on('click', addInput)
-  $('#multiply').on('click', addInput)
-  $('#subtract').on('click', addInput)
-  $('#add').on('click', addInput)
-  $('#dot').on('click', addInput)
-  $('#leftParen').on('click', addInput)
-  $('#rightParen').on('click', addInput)
-  $('#zero').on('click', addInput)
-  $('#one').on('click', addInput)
-  $('#two').on('click', addInput)
-  $('#three').on('click', addInput)
-  $('#four').on('click', addInput)
-  $('#five').on('click', addInput)
-  $('#six').on('click', addInput)
-  $('#seven').on('click', addInput)
-  $('#eight').on('click', addInput)
-  $('#nine').on('click', addInput)
+function onReady() {
+  $("#equal").on("click", postEquation);
+  $("#clear").on("click", clearInputs);
+  $("#clearHistory").on("click", deleteMathHistory);
+  $("#backspace").on("click", backspace);
+  $("#percentage").on("click", percent);
+  $("#divide").on("click", addInput);
+  $("#multiply").on("click", addInput);
+  $("#subtract").on("click", addInput);
+  $("#add").on("click", addInput);
+  $("#dot").on("click", addInput);
+  $("#leftParen").on("click", addInput);
+  $("#rightParen").on("click", addInput);
+  $("#zero").on("click", addInput);
+  $("#one").on("click", addInput);
+  $("#two").on("click", addInput);
+  $("#three").on("click", addInput);
+  $("#four").on("click", addInput);
+  $("#five").on("click", addInput);
+  $("#six").on("click", addInput);
+  $("#seven").on("click", addInput);
+  $("#eight").on("click", addInput);
+  $("#nine").on("click", addInput);
 }
 
 // adds the text of a button to the string in the input field
-function addInput () {
+function addInput() {
   let input = $(this).text();
-  $('#input').val($('#input').val() + input)
-  console.log($('#input').val());
+  $("#input").val($("#input").val() + input);
+  console.log($("#input").val());
 }
 
 // empties the input field
-function clearInputs () {
-  $('#input').val('');
+function clearInputs() {
+  $("#input").val("");
 }
 
 // removes the value at the end of the string
-function backspace () {
+function backspace() {
   let value = $("#input").val();
   $("#input").val(value.slice(0, value.length - 1));
-};
+}
 
-function percent () {
+function percent() {
   let value = $("#input").val();
   let percent = value / 100;
-  $('#input').val(percent)
-  console.log($('#input').val());
+  $("#input").val(percent);
+  console.log($("#input").val());
 }
 
 // takes the equation string and posts it to the server inside an object
 function postEquation() {
-  let input = $('#input').val()
-  $('#input').val('');
+  let input = $("#input").val();
+  $("#input").val("");
   //using AJAX to send a post request to the server
   $.ajax({
-    method: 'POST', 
-    url: '/equation',
+    method: "POST",
+    url: "/equation",
     data: {
       equationToAdd: {
         input: input,
-        equals: '=',
-        answer: '',
-      }
-    }
-  }).then(function(response){
-    console.log('Equation Added');
-    getMathHistory(); // to refresh the DOM with the new item
-  }).catch(function(response){
-    console.log('UGHHHH Equation not added');
+        equals: "=",
+        answer: "",
+      },
+    },
   })
+    .then(function (response) {
+      console.log("Equation Added");
+      getMathHistory(); // to refresh the DOM with the new item
+    })
+    .catch(function (response) {
+      console.log("UGHHHH Equation not added");
+    });
 }
 
 // gets the mathHistory array from the server and posts it to the DOM
-function getMathHistory () {
+function getMathHistory() {
   // using AJAX to make a get request to the server for the scoreboard array
   $.ajax({
-    method: 'GET',
-    url: '/history'
-  }).then(function(response){
-    console.log('History updated', response);
-    // runs renderToDom to append the updated mathHistory array from the server
-    renderToDom(response);
-  }).catch(function(response){
-    console.log('getMathHistory not working', response);
-  });
+    method: "GET",
+    url: "/history",
+  })
+    .then(function (response) {
+      console.log("History updated", response);
+      // runs renderToDom to append the updated mathHistory array from the server
+      renderToDom(response);
+    })
+    .catch(function (response) {
+      console.log("getMathHistory not working", response);
+    });
 }
 
 // used in getMathHistory to post mathHistory to the DOM
 function renderToDom(history) {
   // empties HTML elements before reappending everything
-  $('#mathHistory').empty()
+  $("#mathHistory").empty();
   for (let i of history) {
-    $('#mathHistory').append(`
+    $("#mathHistory").append(`
       <li>${i.firstNumber} ${i.operator} ${i.secondNumber} ${i.equals} ${i.answer}</li>
-    `)
+    `);
   }
 }
 
 // sends a request to the sever to empty the mathHistory array
-function deleteMathHistory () {
+function deleteMathHistory() {
   // using AJAX to make a get request to the server for the scoreboard array
   $.ajax({
-    method: 'DELETE',
-    url: '/history'
-  }).then(function(response){
-    console.log('History deleted', response);
-    // runs renderToDom to append the updated mathHistory array from the server
-    renderToDom(response);
-  }).catch(function(response){
-    console.log('deleteMathHistory not working', response);
-  });
+    method: "DELETE",
+    url: "/history",
+  })
+    .then(function (response) {
+      console.log("History deleted", response);
+      // runs renderToDom to append the updated mathHistory array from the server
+      renderToDom(response);
+    })
+    .catch(function (response) {
+      console.log("deleteMathHistory not working", response);
+    });
 }
-
-
-
-
-
--- server/server.js
+++ server/server.js
@@ -1,95 +1,83 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 5000;
 
 //body parser
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // points to where the html file is
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 
-let mathHistory = []
-
+let mathHistory = [];
 let firstNumber = 0;
 let operator = '';
 let secondNumber = 0;
 
-app.post('/equation', function(req, res){
-  // the data that is send from the client is saved for us in req.body
-  // console.log('req.body from the POST is', req.body);
-  
-  // sets the elements of the object sent by the client to values
-  // let firstNumber = Number(req.body.equationToAdd.firstNumber);
-  // let secondNumber = Number(req.body.equationToAdd.secondNumber);
-  // let operator = req.body.equationToAdd.operator;
-  // let equals = req.body.equationToAdd.equals;
-  
-  let equationString = (req.body.equationToAdd.input)
+app.post("/equation", function (req, res) {
+  let equationString = req.body.equationToAdd.input;
   console.log(equationString);
 
-  // enters the values into a function that does the math
-  // and returns an answer
-  // let answer = doMath(firstNumber, operator, secondNumber)
-
-  // let answer = doMath()
-  
-  let answer = doMathDifferently(equationString)
-
+  let answer = doMathDifferently(equationString);
   console.log(answer);
 
   // adds the answer element to the object
-  req.body.equationToAdd['firstNumber'] = firstNumber;
-  req.body.equationToAdd['secondNumber'] = secondNumber;
-  req.body.equationToAdd['operator'] = operator;
-  req.body.equationToAdd['answer'] = answer;
+  req.body.equationToAdd["firstNumber"] = firstNumber;
+  req.body.equationToAdd["secondNumber"] = secondNumber;
+  req.body.equationToAdd["operator"] = operator;
+  req.body.equationToAdd["answer"] = answer;
 
-  // // pushes the updated object with the answer to 
-  // // the array that records each equation
+  // pushes the updated object with the answer to the mathHistory array
   mathHistory.push(req.body.equationToAdd);
 
   // send back a status code of 201
   res.sendStatus(201);
-})
+});
 
 // gets the current history and sends it to the client
-app.get('/history', function(req, res){
+app.get("/history", function (req, res) {
   res.send(mathHistory);
 });
 
-app.delete('/history', function(req, res){
+// empties the mathHistory array
+app.delete("/history", function (req, res) {
   mathHistory = [];
   res.send(mathHistory);
 });
 
-function doMathDifferently (str) {
-  let numOne = '';
-  let symbol = '';
-  let numTwo = '';
+function doMathDifferently(str) {
+  let numOne = "";
+  let symbol = "";
+  let numTwo = "";
 
-  for (let i=0; i<str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     if (!isNaN(String(str[i]) * 1)) {
       numOne += str[i];
     }
-    if (String(str[i]) === '.' ) {
+    if (String(str[i]) === ".") {
       numOne += str[i];
     }
-    if (String(str[i]) === '/' || str[i] === '*' || str[i] === '-' || str[i] === '+'){
+    if (
+      String(str[i]) === "/" ||
+      str[i] === "*" ||
+      str[i] === "-" ||
+      str[i] === "+"
+    ) {
       symbol = str[i];
       break;
     }
   }
 
-  for (let i=numOne.length + 1; i<str.length; i++) {
+  for (let i = numOne.length + 1; i < str.length; i++) {
     if (!isNaN(String(str[i]) * 1)) {
       numTwo += str[i];
     }
-    if (String(str[i]) === '.' ) {
+    if (String(str[i]) === ".") {
       numTwo += str[i];
     }
   }
-  
+
   console.log(numOne);
   console.log(symbol);
   console.log(numTwo);
@@ -98,33 +86,27 @@ function doMathDifferently (str) {
   operator = symbol;
   secondNumber = numTwo;
 
-  return doMath(numOne, symbol, numTwo)
+  return doMath(numOne, symbol, numTwo);
 }
 
-
-function doMath (first, operator, second) {
-  if (operator === '/') {
+function doMath(first, operator, second) {
+  if (operator === "/") {
     // console.log('doMath is running');
     let answer = first / second;
-    return answer; 
-  }
-  else if (operator === '*') {
+    return answer;
+  } else if (operator === "*") {
     let answer = first * second;
-    return answer; 
-  }
-  else if (operator === '-') {
+    return answer;
+  } else if (operator === "-") {
     let answer = first - second;
-    return answer; 
-  }
-  else if (operator === '+') {
+    return answer;
+  } else if (operator === "+") {
     let answer = Number(first) + Number(second);
-    return answer; 
+    return answer;
   }
 }
 
-
-
 //listens for the port and starts our server
-app.listen(PORT, function(){
-  console.log('listening on port', PORT);
+app.listen(PORT, function () {
+  console.log("listening on port", PORT);
 });
