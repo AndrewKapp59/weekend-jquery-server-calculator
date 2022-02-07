a5669bf585fac0ba3c5430460a75aabc03fdea73
Andrew Kapp <dkapp59@gmail.com>
2022-02-07 16:54:00 -0600

    updated port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: true }));
