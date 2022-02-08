55674e21c0bebb9fdeb64c5f2fab89a5dabc9303
Andrew Kapp <dkapp59@gmail.com>
2022-02-07 18:27:28 -0600

    port changed to 4000

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.port || 4000;
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: true }));
